e06749c085b83c1cfd1afeab2006db9b8cb76875
EfeHasircioglu <efehasircioglu.com@gmail.com>
2025-09-19 00:36:24 +0300

    finished product, just needs some handmade AnimatePresence magic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,70 +7,96 @@ function App() {
   //şuan elimizdeki textin verisini global olarak tutabilmek için
   const [currentData, setCurrentData] = useState("");
   //modal açık mı değil mi onu yönetebilmek için bir state
-  const [isModalOpen, setIsModalOpen] = useState(true);
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  //doğruluk durumuna göre farklı yazı gösterecek, bunu Modal componentine göndereceğiz
+  const [isCorrect, setIsCorrect] = useState(undefined);
   async function getData() {
     const resp = await fetch("http://localhost:3000/prompt");
     const { id, text, isAi } = await resp.json();
     setCurrentData({ id, text, isAi });
     console.log(id, text, isAi); //! DEBUG
   }
+  function toggleModal() {
+    if (isModalOpen) {
+      setIsModalOpen(false);
+    } else {
+      setIsModalOpen(true);
+    }
+  }
   const [score, setScore] = useState(0);
   function checkText(button) {
     if (button === "ai") {
       if (currentData.isAi) {
-        //TODO: Modal'daki doğru bilme durumu olacak
+        //Modal'daki doğru bilme durumu
+        setIsCorrect(true);
+        setScore((prev) => prev + 1);
         //sunucudan yeni bir yazı istemek için
         getData();
       } else {
-        //TODO: modaldaki yanlış bilme durumu
+        //modaldaki yanlış bilme durumu
+        setIsCorrect(false);
         //sunucudan yeni yazı
         getData();
       }
     } else if (button === "human") {
       if (!currentData.isAi) {
-        //TODO: doğru bilme durumu
+        // doğru bilme durumu
+        setIsCorrect(true);
+        setScore((prev) => prev + 1);
         //sunucudan yeni yazı
         getData();
       } else {
-        //TODO: yanlış bilme durumu
+        // yanlış bilme durumu
+        setIsCorrect(false);
         //sunucudan yeni yazı
         getData();
       }
     }
+    setIsModalOpen(true);
   }
   //uygulamaya ilk girdiğimizde yazı gözüksün
   useEffect(() => {
     getData();
   }, []);
   return (
-    <div className="bg-slate-900 w-screen">
-      <div className="max-w-[1000px] justify-self-center flex-col h-screen text-white p-5">
-        <div className="text-3xl font-bold mb-2 justify-self-center">
-          Is the text AI generated?
-        </div>
-        <div className="p-4 border-1 bg-slate-800 text-lg border-dashed rounded-2xl mt-4">
-          {currentData.text}
-        </div>
-        <div className="flex flex-col">
-          <button
-            onClick={() => checkText("ai")}
-            className="w-full p-3 cursor-pointer text-lg hover:bg-blue-800 transition-colors mt-5 rounded-2xl bg-blue-600"
-          >
-            This text is written by AI. 🤖
-          </button>
-          <button
-            onClick={() => checkText("human")}
-            className="w-full p-3 cursor-pointer text-lg hover:bg-rose-800 transition-colors mt-5 rounded-2xl bg-rose-600"
-          >
-            This text is written by a human. 🙋‍♀️
-          </button>
-        </div>
-        <div className="font-mono my-5 px-3 py-1 text-xl bg-slate-800 w-fit rounded-xl">
-          Score will go here
+    <>
+      <div className="bg-slate-900 w-screen">
+        <div className="max-w-[1000px] justify-self-center flex-col h-screen text-white p-5">
+          <div className="text-3xl font-bold mb-2 justify-self-center">
+            Is the text AI generated?
+          </div>
+          <div className="w-full">
+            {isModalOpen ? (
+              <Modal isCorrect={isCorrect} toggleModal={toggleModal}></Modal>
+            ) : (
+              <>
+                <div className="p-4 border-1 bg-slate-800 text-lg border-dashed rounded-2xl mt-4">
+                  {currentData.text}
+                </div>
+                <div className="flex flex-col">
+                  <button
+                    onClick={() => checkText("ai")}
+                    className="w-full p-3 cursor-pointer text-lg hover:bg-blue-800 transition-colors mt-5 rounded-2xl bg-blue-600"
+                  >
+                    This text is written by AI. 🤖
+                  </button>
+                  <button
+                    onClick={() => checkText("human")}
+                    className="w-full p-3 cursor-pointer text-lg hover:bg-rose-800 transition-colors mt-5 rounded-2xl bg-rose-600"
+                  >
+                    This text is written by a human. 🙋‍♀️
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+
+          <div className="font-mono my-5 px-3 py-1 text-xl bg-slate-800 w-fit rounded-xl">
+            Score is {score}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- frontend/src/Modal.jsx
+++ frontend/src/Modal.jsx
@@ -1,6 +1,21 @@
 import { useState } from "react";
 
-export default function Modal(isOpen, isCorrect) {
-  /* isOpen durumu ve isCorrect durumu olacak */
-  return "";
+export default function Modal({ toggleModal, isCorrect }) {
+  /* closeModal fonksiyonu ve isCorrect durumu olacak */
+  return (
+    <div className="text-lg w-full py-10 text-center flex flex-col gap-3 bg-slate-900 ">
+      <div>
+        {isCorrect
+          ? "✔️ Congratulations! You are right."
+          : "❌ Your answer is wrong, better luck next time!"}
+      </div>
+      <button
+        className="w-full
+    justify-self-center p-2 cursor-pointer bg-blue-600 hover:bg-blue-800 transition-colors text-white rounded-xl"
+        onClick={() => toggleModal()}
+      >
+        Continue
+      </button>
+    </div>
+  );
 }
