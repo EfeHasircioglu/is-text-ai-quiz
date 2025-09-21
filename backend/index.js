75bde62720d0cb154a5441cbf51e7946eb3589d0
EfeHasircioglu <efehasircioglu.com@gmail.com>
2025-09-22 00:57:43 +0300

    changes for railway deployment

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import fs from "fs";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 const data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
-
+//frontendi express'den sunuyorum
+app.use(express.static(path.join(process.cwd(), ".../frontend/dist")));
 // GET /prompt ile JSON dosyasından rastgele bir yazı döndürüyoruz
 app.get("/prompt", (req, res) => {
   const random = data[Math.floor(Math.random() * data.length)]; //random, data.json'daki objelerden, yani yazılardan bir tanesi
@@ -14,5 +16,9 @@ app.get("/prompt", (req, res) => {
   res.json({ id: random.id, text: random.text, isAi: random.isAi });
 });
 
-const PORT = 3000;
+//react için catch all handler
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "../frontend/dist", "index.html"));
+});
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
